a657a515edc4ca9c2482fc686d930b807d2fec91
Faizal Rohmat <immaddudin@gmail.com>
2024-01-14 10:02:16 +0700

    Made parallel code

--- GSMaP_analyze.R
+++ GSMaP_analyze.R
@@ -2,14 +2,14 @@ rm(list = ls())
 
 library(tidyverse)
 library(terra)
-library(lubridate)
 library(parallel)
+library(tictoc)
 
 gsmap_zip_dir = "data/GSMaP_v7_hourly_MVK_zip/"
 gsmap_crop_dir = "results/GSMaP_v7_hourly_MVK_crop/"
 
-reference_raster = terra::rast("./data/reference_raster.tif")
-crop_raster = terra::rast("./data/indonesia_crop_raster.tif")
+reference_raster_path = "./data/reference_raster.tif"
+crop_raster_path = "./data/indonesia_crop_raster.tif"
 
 gsmap_zip_files = 
   tibble(Path = list.files(gsmap_zip_dir, recursive = TRUE, full.names = TRUE)) |> 
@@ -24,23 +24,46 @@ lat = seq(59.95, -59.95, length.out = 1200)
 raster_grid = tibble(expand.grid(x = lon, y = lat)) |>
   mutate(x = if_else(x > 180, x - 360, x))
 
-gsmap_unzip_files = 
-  lapply(1:dim(gsmap_zip_files)[[1]], function(i){
-  zippath = gsmap_zip_files$Path[i]
-  
-  con = gzcon(file(zippath, "rb"))
-  zipvalues = readBin(con, double(), n = 1200 * 3600, size = 4, endian = "little")
-  zipvalues[zipvalues < 0] = NA
-  close(con)
-  
-  raster_content = cbind(raster_grid, zipvalues)
-  this_raster = terra::rast(raster_content, crs = crs(reference_raster))
-  cropped_raster = terra::crop(this_raster, crop_raster)
-  
-  savepath = paste0(file.path(gsmap_crop_dir, basename(tools::file_path_sans_ext(zippath))), ".tif")
-  
-  dir.create(dirname(savepath))
-  
-  writeRaster(cropped_raster, savepath)
+tic()
+
+cl = makeCluster(pmax(1, floor(detectCores() * 2/3)))
+clusterExport(cl, c("gsmap_zip_files", "raster_grid", "reference_raster_path",
+                    "crop_raster_path", "gsmap_crop_dir"))
+clusterEvalQ(cl, {
+  library(tidyverse)
+  library(terra)
 })
 
+gsmap_unzip_files = 
+  parLapply(cl, 1:dim(gsmap_zip_files)[[1]], function(i){
+    
+    reference_raster = terra::rast(reference_raster_path)
+    crop_raster = terra::rast(crop_raster_path)
+    
+    zippath = gsmap_zip_files$Path[i]
+    
+    con = gzcon(file(zippath, "rb"))
+    zipvalues = readBin(con, double(), n = 1200 * 3600, size = 4, endian = "little")
+    zipvalues[zipvalues < 0] = NA
+    close(con)
+    
+    raster_content = cbind(raster_grid, zipvalues)
+    this_raster = terra::rast(raster_content, crs = crs(reference_raster))
+    cropped_raster = terra::crop(this_raster, crop_raster)
+    
+    savepath = paste0(file.path(gsmap_crop_dir, 
+                                gsmap_zip_files$Year,
+                                basename(tools::file_path_sans_ext(zippath))),
+                      ".tif")
+    
+    dir.create(dirname(savepath))
+    
+    if (!file.exists(savepath)) {
+      writeRaster(cropped_raster, savepath)
+    }
+    
+})
+
+stopCluster(cl)
+
+toc()
